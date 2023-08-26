cfa2be971e1ff56eeb5c49a38c80e3c09225345d
Tacettin Baran <tacettinbaran1@gmail.com>
2023-08-27 01:11:58 +0300

    First commit

--- app/src/main/java/com/baran/cleanmovieapp/util/Constants.kt
+++ app/src/main/java/com/baran/cleanmovieapp/util/Constants.kt
@@ -9,6 +9,7 @@ object Constants {
     Click the following URL to activate your key: http://www.omdbapi.com/apikey.aspx?VERIFYKEY=d2bb5baa-8bfd-4b09-87b0-85225b5421c1
     If you did not make this request, please disregard this email.
      */
+
     const val BASE_URL = "http://www.omdbapi.com/"
     const val API_KEY = "f2e3d529"
     const val IMDB_ID = "imdb_id"
